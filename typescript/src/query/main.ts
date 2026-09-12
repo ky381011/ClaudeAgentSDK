718f39e24a5d2c875cc0569dafb4bb831dc5e3d1
ky381011 <ky381011git@gmail.com>
2026-09-12 16:19:55 +0900

    Add test in function

--- typescript/src/query/main.ts
+++ typescript/src/query/main.ts
@@ -33,3 +33,27 @@ async function runQuery(content: QueryUnit): Promise<string> {
 
   throw new Error("Query result not found")
 }
+
+async function main() {
+  console.log(`\x1b[42m1. Unit名表示テスト\x1b[0m`)
+  console.log()
+
+  const testMessage: string = "動作テスト"
+  displayUnitName(testMessage)
+
+  console.log(`\x1b[42m2. Query実行テスト\x1b[0m`)
+  console.log()
+
+  const testQuery: QueryUnit = {
+    name: "クエリ実行テスト",
+    prompt: "こんにちは。あなたは何ができますか？",
+  }
+
+  const result = await runQuery(testQuery)
+
+  console.log(result)
+}
+
+if (require.main === module) {
+  main()
+}
