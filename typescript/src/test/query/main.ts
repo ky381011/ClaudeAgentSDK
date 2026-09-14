2dfbcf5754d5a8e33555bac205119bcccdfd5be1
ky381011 <ky381011git@gmail.com>
2026-09-14 20:44:56 +0900

    Update test: Enable to choose test

--- typescript/src/test/query/main.ts
+++ typescript/src/test/query/main.ts
@@ -2,13 +2,15 @@ import { QueryUnit } from "../../unit/interfaces/query";
 import { displayUnitName } from "../../query/main";
 import { runQuery } from "../../query/main";
 
-async function main() {
+async function testDisplayUnitName() {
   console.log(`\x1b[42m1. Unit名表示テスト\x1b[0m`)
   console.log()
 
   const testMessage: string = "動作テスト"
   displayUnitName(testMessage)
+}
 
+async function testRunQuery() {
   console.log(`\x1b[42m2. Query実行テスト\x1b[0m`)
   console.log()
 
@@ -20,6 +22,21 @@ async function main() {
   const result = await runQuery(testQuery)
 
   console.log(result)
+}
+
+async function main() {
+  const args = process.argv.slice(2)
+  const testNumber = args[0]
+
+  if (testNumber === "1") {
+    await testDisplayUnitName()
+  } else if (testNumber === "2") {
+    await testRunQuery()
+  } else {
+    await testDisplayUnitName()
+    console.log()
+    await testRunQuery()
+  }
 }
 
 if (require.main === module) {
