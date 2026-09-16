7f6b908508247234a7963b3f259d4116e6e57eee
ky381011 <ky381011git@gmail.com>
2026-09-16 23:29:40 +0900

    Modify script: Add arithmetic function

--- utility/python/calcurator/arithmetic.py
+++ utility/python/calcurator/arithmetic.py
@@ -1,2 +1,16 @@
-def add(a: int, b:int):
-  return a + b
+def add(a: float, b: float) -> float:
+    return a + b
+
+
+def subtract(a: float, b: float) -> float:
+    return a - b
+
+
+def multiply(a: float, b: float) -> float:
+    return a * b
+
+
+def divide(a: float, b: float) -> float:
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
