11d65f277ad14180a76aecb547c8e5e221876336
ky381011 <ky381011git@gmail.com>
2026-09-22 21:04:40 +0900

    Enable to run python script

--- typescript/src/script/inMethod.ts
+++ typescript/src/script/inMethod.ts
@@ -1,30 +1,37 @@
 import { execFile } from "node:child_process"
 import { promisify } from "node:util"
+import path from "node:path"
+
+// プロジェクトへの相対パス
+// NOTE : このファイルからではなく npx コマンドを実行する場所からの相対パス
+const projectRootPath = "../../"
+// プロジェクトからの相対パス
+const scriptPath = "utility/python/"
+
+const mainPath = path.resolve(
+  projectRootPath,
+  scriptPath,
+  "main.py",
+)
 
 const execFileAsync = promisify(execFile)
-const pythonPath = "utility/python/calcurator"
 
-async function callPythonFunction(
-  functionName: string,
-  args: number[]
-): Promise<number> {
-  const argsStr = args.join(",")
-  try {
-    const { stdout } = await execFileAsync("python", [
-      "-c",
-      `
-import sys
-sys.path.insert(0, '${pythonPath}')
-from arithmetic import ${functionName}
-result = ${functionName}(${argsStr})
-print(result)
-`,
-    ])
-    return parseFloat(stdout.trim())
-  } catch (error) {
-    console.error(`Error calling ${functionName}:`, error)
-    throw error
-  }
+const args = [
+  "calcurator",
+  "arithmetic",
+  "add",
+  "10",
+  "5"
+]
+
+async function main() {
+  const { stdout } = await execFileAsync(
+    "python",
+    [mainPath, ...args],
+  )
+
+  console.log(stdout)
 }
 
-export { callPythonFunction }
+main()
+export {}
